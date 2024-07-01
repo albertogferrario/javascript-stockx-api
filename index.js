e81fea795832fd4d479bd1cfd92c7cd8d072a212
Alberto Giancarlo Ferrario <me@albertogferrar.io>
2024-07-01 20:37:46 +0700

    add jwt update logic

--- index.js
+++ index.js
@@ -6,25 +6,43 @@ const {
 const Client = require("./src/client");
 const Limiter = require("./src/limiter");
 
+function makeClient(apiKey, jwt, limiter) {
+  return new Client(
+    BASE_URL,
+    {
+      Accept: 'application/json',
+      'x-api-key': apiKey,
+      Authorization: `Bearer ${jwt}`,
+    },
+    limiter,
+  );
+}
+
+function makeLimiter() {
+  return new Limiter(
+    REQUEST_RATE_LIMIT_MIN_TIME,
+    REQUEST_RATE_LIMIT_RESERVOIR_AMOUNT,
+    REQUEST_RATE_LIMIT_RESERVOIR_REFRESH_CRON_EXPRESSION,
+  );
+}
+
 class StockxApi {
   constructor(apiKey, jwt) {
-    const limiter = new Limiter(
-      REQUEST_RATE_LIMIT_MIN_TIME,
-      REQUEST_RATE_LIMIT_RESERVOIR_AMOUNT,
-      REQUEST_RATE_LIMIT_RESERVOIR_REFRESH_CRON_EXPRESSION,
-    );
+    this.apiKey = apiKey;
+    this.jwt = jwt;
+    this.limiter = makeLimiter();
 
-    const client = new Client(
-      BASE_URL,
-      {
-        Accept: 'application/json',
-        'x-api-key': apiKey,
-        Authorization: `Bearer ${jwt}`,
-      },
-      limiter,
-    );
+    this.initResources(makeClient(this.apiKey, this.jwt, this.limiter))
+  }
 
+  initResources(client) {
     this.catalog = new Catalog(client);
+  }
+
+  updateJwt(jwt) {
+    this.jwt = jwt;
+
+    this.initResources(makeClient(this.apiKey, this.jwt, this.limiter));
   }
 }
 
