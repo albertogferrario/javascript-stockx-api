43f1d248cf920058643f64455c8e605fa61cf8c9
Alberto Giancarlo Ferrario <me@albertogferrar.io>
2024-07-06 16:21:58 +0700

    add request timeout handling

--- src/client.js
+++ src/client.js
@@ -1,5 +1,7 @@
 const axios = require("axios").default;
 const {Axios} = require("axios");
+const {timeoutPromise} = require("./utilities");
+const {REQUEST_TIMEOUT} = require("../config");
 
 class Client extends Axios {
   constructor(baseUrl, headers, limiter) {
@@ -12,13 +14,21 @@ class Client extends Axios {
     this.limiter = limiter;
   }
 
-  get = async (url, config) => await this.limiter.schedule(() => super.get(url, config));
+  get = async (url, config) => {
+    return await this.limiter.schedule(() => timeoutPromise(super.get(url, config), REQUEST_TIMEOUT));
+  };
 
-  post = async (url, data, config) => await this.limiter.schedule(() => super.post(url, data, config));
+  post = async (url, data, config) => {
+    return await this.limiter.schedule(() => timeoutPromise(super.post(url, data, config), REQUEST_TIMEOUT));
+  };
 
-  put = async (url, data, config) => await this.limiter.schedule(() => super.put(url, data, config));
+  put = async (url, data, config) => {
+    return await this.limiter.schedule(() => timeoutPromise(super.put(url, data, config), REQUEST_TIMEOUT));
+  };
 
-  delete = async (url, config) => await this.limiter.schedule(() => super.delete(url, config));
+  delete = async (url, config) => {
+    return await this.limiter.schedule(() => timeoutPromise(super.delete(url, config), REQUEST_TIMEOUT));
+  };
 }
 
 module.exports = Client;
--- src/utilities.js
+++ src/utilities.js
@@ -4,6 +4,15 @@ function getMillisecondsToNextCronExpressionTick(cronExpression) {
   return cronParser.parseExpression(cronExpression).next().toDate() - (new Date());
 }
 
+async function timeoutPromise(promise, timeout) {
+  const timeoutPromise_ = new Promise((_, reject) =>
+    setTimeout(() => reject(new Error('Request timed out')), timeout)
+  );
+
+  return Promise.race([promise, timeoutPromise_]);
+}
+
 module.exports = {
   getMillisecondsToNextCronExpressionTick,
+  timeoutPromise,
 };
